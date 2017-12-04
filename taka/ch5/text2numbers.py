d3aac44edaeeb895dc4b4f2253479d9e7d0a6433
atfam <2115106u@g.hit-u.ac.jp>
2017-12-04 12:19:01 -0600

    [update]5.4.2

--- taka/ch5/text2numbers.py
+++ taka/ch5/text2numbers.py
@@ -2,12 +2,9 @@ def main():
     print("This program converts a textual message into a sequence")
     print("of numbers responding the Unicode encoding of the message.\n")
 
-    message = input("Please enter the message to encode: ")
+    message = input("Please enter the message to decode(Please divide the numbers with spaces): ")
     print("\nHere are the Unicode codes: ")
-    for ch in message.split(" "):
-        print(ch)
-        print(chr(int(ch)), end="")
-
-    print()
+    for ch in message.split():
+        print(chr(eval(ch)), end="")
 
 main()
