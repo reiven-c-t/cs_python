d92057dff7b47395224170a605a60f465065cd5f
reiven <seventhtrigger0@gmail.com>
2017-12-13 17:55:52 -0600

    [create] 課題関連のinitial commit

--- aki/question/data_generator.py
+++ aki/question/data_generator.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
-from random import choice, uniform
+from random import choice, uniform, seed
 import codecs
+
+seed(1)
 
 
 def generate_csv(length, colHeaderList):
@@ -27,7 +29,7 @@ def generate_csv(length, colHeaderList):
                 col.append(choice(["good", "bad", "soso", "awesome", "tired", "exhausted"]))
             elif item == "climate":
                 col.append("%.3f" % uniform(-10, 30))
-            else:  # ミスってるけどまあいいや
+            elif item.find("random") != -1:  # ミスってるけどまあいいや
                 dummyList = [
                     ["りんご", "ぶどう", "なし", "オレンジ", "みかん", "パイナッポー", "ペン"],
                     ["ハンバーガー", "牛丼", "天丼", "とん丼", "トンカツ", "ししゃも"],
@@ -35,7 +37,7 @@ def generate_csv(length, colHeaderList):
                     ["腹筋", "背筋", "ダンベルカール", "アームカール", "デッドリフト", "ラットプルダウン", "プルダウン"],
                     ["温泉", "登山", "海水浴", "ボートのり", "美術館巡り", "カフェ巡り"]
                 ]
-                col.append(choice(choice(dummyList)))
+                col.append(choice(dummyList[int(item.replace("random", ""))]))
         result += ", ".join(col) + "\n"
         count += 1
     return result
@@ -44,7 +46,7 @@ def generate_csv(length, colHeaderList):
 if __name__ == '__main__':
     csv_header = ["date", "day", "weather", "condition", "climate", "random1", "random2", "random3"]
     csv_raw_data = generate_csv(10000,
-                                ["date", "day", "wather", "condition", "climate", "random1", "random2", "random3"])
+                                ["date", "day", "weather", "condition", "climate", "random1", "random2", "random3"])
 
     csv_raw_data = csv_raw_data.split("\n")
 
@@ -54,19 +56,41 @@ if __name__ == '__main__':
         csv_last.append(row)
     csv_last = "\n".join(csv_last)
 
-    with codecs.open("large_test_1.csv", "w", "shift-jis") as file:
+    with codecs.open("large1_shift-jis.csv", "w", "shift-jis") as file:
         file.write(csv_first)
-    with codecs.open("large_test_2.csv", "w", "shift-jis") as file:
+        print("success")
+    with codecs.open("large2_shift-jis.csv", "w", "shift-jis") as file:
         file.write(csv_last)
-"""
-課題ラフ
-- 共通演算(day一致検知から配列生成=>join()
-- 差演算(データ - 共通部分のday不一致から配列生成=>join()
-- 和集合演算(データ1の差演算集合+データ2)
-- 選択演算(test_1にて気分等号X,気温Y以上,random_3食事等号条件Z)の行を選択する関数設計からの
-    気分bad,気温0度以上、トンカツを食べた日の行数を出力
-- 射影演算(csvの列の1部のみを抽出する関数を作成(headerは与える))その関数を使って選択演算での上記の際のdateとweatherのみのcsvを作成)
 
-気分はbad,気温0度以上でトンカツを食べた日をlarge_test_1.csvの個数(22件)
+    with codecs.open("large_test_1_utf-8.csv", "w", "utf-8") as file:
+        file.write(csv_first)
+        print("success")
+    with codecs.open("large_test_2_utf-8.csv", "w", "utf-8") as file:
+        file.write(csv_last)
 
-"""
+
+    # small data
+    csv_header = ["date", "day", "weather", "condition", "climate", "random1", "random2", "random3"]
+    csv_raw_data = generate_csv(20,
+                                ["date", "day", "weather", "condition", "climate", "random1", "random2", "random3"])
+
+    csv_raw_data = csv_raw_data.split("\n")
+
+    csv_first = "\n".join(csv_raw_data[0:12])
+    csv_last = [", ".join(csv_header)]
+    for row in csv_raw_data[10:]:
+        csv_last.append(row)
+    csv_last = "\n".join(csv_last)
+
+    with codecs.open("small1_shift-jis.csv", "w", "shift-jis") as file:
+        file.write(csv_first)
+        print("success")
+    with codecs.open("small2_shift-jis.csv", "w", "shift-jis") as file:
+        file.write(csv_last)
+
+    with codecs.open("small_test_1_utf-8.csv", "w", "utf-8") as file:
+        file.write(csv_first)
+        print("success")
+    with codecs.open("small_test_2_utf-8.csv", "w", "utf-8") as file:
+        file.write(csv_last)
+
