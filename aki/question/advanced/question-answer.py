7ede3e5313308e3eb78aa5f64b88b3539d5bd489
reiven <seventhtrigger0@gmail.com>
2017-12-21 19:11:29 -0600

    [temp] advancedなquestionを作成してる途中

--- aki/question/advanced/question-answer.py
+++ aki/question/advanced/question-answer.py
@@ -2,12 +2,12 @@
 すること:
 1. testの0~9のフォルダの中から適当に1個csvを選ぶ
 2. 選んだやつをadvancedフォルダに移動 or コピー
+
 """
 import codecs
 import os
 
 
-# write your own csv_reader function that load csv, or use following function. Anyway result is same.
 def csv_reader(csv_file):
     # simply load csv file and split it with return
     # or you could write your own loader.
@@ -15,42 +15,36 @@ def csv_reader(csv_file):
 
 
 # 共通部分演算
-# use your own intersect() or use following function. Anyway, result is same.
 def intersect(csv1, csv2):
     return list(set(csv1).intersection(set(csv2)))  # should sort and remove header but enough
 
 
 # 和集合演算
-# use your own union() or use following function. Anyway, result is same.
 def union(csv1, csv2):
     return list(set(csv1).union(set(csv2)))  # should sort and fix header position but enough.
 
 
 def jsc(csv1, csv2):
-    # write your own jsc()
-    #
-    # But again, what you need to do is
-    # divide amount of intersection component of csv1 and csv2
-    # and amount of union component of csv1 and csv2
-    #
-    # Then, you know what to do?
-    pass
+    return len(intersect(csv1, csv2)) / len(union(csv1, csv2))
 
 
 def classifier(csv):
-    for label in range(10):  # folder's label.
-        top_dir_path = "train" + os.sep + "%d" % label
-        for root, dirs, files in os.walk(top_dir_path):  # TODO: Might be OK. But I could not check on Windows.
-            # if you need, remove comment below and check what the os.walk() do.
-            # or see the document.
-            #
+    max_jsc = 0
+    current_label = 0
+    for label in range(10):
+        top_dir_path = "train" +os.sep +"%d" % label
+        for root, dirs, files in os.walk(top_dir_path): # TODO: Mightbe
+            # if you need, comment out below and check what the os.walk() do.
             # print('-' * 10)
             # print('root:{}'.format(root))
             # print('dirs:{}'.format(dirs))
             # print('files:{}'.format(files))
             for file in files:
-                # csv_path is the path for each csv file path in test/ folder.
-                csv_path = top_dir_path + os.sep + file
+                temp_jsc = jsc(csv, csv_reader(top_dir_path + os.sep + file))
+                if max_jsc <= temp_jsc:
+                    max_jsc = temp_jsc
+                    current_label = label
+    return current_label
 
 
 if __name__ == '__main__':
