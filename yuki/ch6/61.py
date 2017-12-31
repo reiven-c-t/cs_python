a998c6dfb8734370056e1bdbef9e62f292a4abbd
YagiBei <overcome924@gmail.com>
2018-01-01 00:38:15 +0100

    1-1-2018-1

---
+++ yuki/ch6/61.py
@@ -0,0 +1,29 @@
+def main():
+    print("10 year investment")
+    principal= eval(input("initial principal:"))
+    apr=eval(input("anualized interest rate:"))
+
+    win=GraphWin("Invest growth chart",320.240)
+    win.setBackground("white")
+    win.setCoords(-1.75,-200,11.5,10400)
+    Text(Point(-1,0 ), ' 0.0K').draw(win)
+    Text(Point(-1, 2500), ' 2.5K').draw(win)
+    Text(Point(-1, 5000), ' 5.0K').draw(win)
+    Text(Point(-1, 7500), ' 7.5K').draw(win)
+    Text(Point(-1, 10000), ' 10.0K').draw(win)
+
+    bar=Rectangle(Point(0,0),Point(1,principal))
+    bar.setFill("green")
+    bar.setWidth(2)
+    bar.draw(win)
+
+    for year in range(1,11):
+        principal=principal*(1+apr)
+        bar = Rectangle(Point(year, 0), Point(year+1, principal))
+        bar.setFill("green")
+        bar.setWidth(2)
+        bar.draw(win)
+    input("push enter")
+    win.close()
+
+main()
