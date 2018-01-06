c27346ceb75ecf6f08b6c6536f06ada93cf1f8a6
YagiBei <overcome924@gmail.com>
2018-01-06 20:19:57 +0100

    6-1-2018-3

--- yuki/ch12/12-1.py
+++ yuki/ch12/12-1.py
@@ -5,3 +5,20 @@
 
 #332p
 
+#theGame = RBallGame(proA,proB)
+#theGame.play()
+
+def main():
+    #printIntro()
+    print()
+    proA,proB,n=getInputs()
+    #
+    stats=simStats()
+    for i in range(n):
+        theGame = RBallGame(proA, proB)
+        theGame.play()
+        stats.update(theGame)
+    stats.printReport()
+
+
+main()
---
+++ yuki/ch12/12-2.1.py
@@ -0,0 +1,8 @@
+##6-1-2018
+
+#337p
+
+def printReport(self):
+    #
+    n = self.winsA+self.winsB
+    print("summary of ",n,"games:\n")
