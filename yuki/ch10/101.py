34695622899ad35dbdd2c1ae385669a013f645c4
YagiBei <overcome924@gmail.com>
2018-01-06 20:03:37 +0100

    6-1-2018-2

---
+++ yuki/ch10/101.py
@@ -0,0 +1,8 @@
+##roc
+##101
+##YUKI_YAGI
+##6-1-2018
+
+#256p
+
+myCircle = Circle(Point(0,0),20)
--- yuki/ch7/71.py
+++ yuki/ch7/71.py
@@ -3,8 +3,26 @@
 ##YUKI_YAGI
 ##6-1-2018
 
-def main():
-    cel = eval(input("<type>celsius temperature: "))
-    fah =  9/5* cel + 32
-    print("fahrenheit: ", fah, "F")
-main()
+#def main():
+ #   cel = eval(input("<type>celsius temperature: "))
+  #  fah =  9/5* cel + 32
+   # print("fahrenheit: ", fah, "F")
+##main()
+
+#def main():
+#    cel = eval(input("<type>celsius temperature: "))
+#    fah =  9/5* cel + 32
+#    print("fahrenheit: ", fah, "F")
+#    if fah > 90:
+#        print("hot")
+#    if fah <30:
+#        print("cold")
+#main()
+import math
+math.__name__
+
+3>4
+print("hello"<"hello")
+
+#hello<hello
+
