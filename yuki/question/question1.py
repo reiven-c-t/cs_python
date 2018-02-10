628e13fed32d2d1e8c6c01914f1c99250dad8cc2
YagiBei <overcome924@gmail.com>
2018-02-10 08:06:19 -0600

    [update]kadai

--- yuki/question/question1.py
+++ yuki/question/question1.py
@@ -30,38 +30,49 @@ csv_2 = codecs.open("small2_shift-jis.csv", "r", "shift-jis")
 csv_1 = csv_1.readlines()
 csv_2 = csv_2.readlines()
 
-print("CSV1: \n", csv_1)
-print("CSV2: \n", csv_2)
+
+# print("CSV1: \n", csv_1)
+# print("CSV2: \n", csv_2)
 
 
 # 差演算
 def difference(csv1, csv2):
-    result =[]
+    result = []
     for line1 in csv1:
         for line2 in csv2:
-            flag= False
+            flag = False
             if line1 == line2:
                 result.append(line1)
                 flag = True
-                if flag = True:
-                  break
+            #if flag == True:
+        #break
 
     return result
 
 
-
-
-
 # 共通部分演算
 def intersect(csv1, csv2):
-    pass  # この行を消以下略
+    result = []
+    for line1 in csv1:
+        for line2 in csv2:
+            flag = False
+            if line1 != line2:
+                result.append(line1)
+                flag = True
+                # if flag == True:
+                # break
+
+            print(line2)
 
 
 # 和集合演算
 def union(csv1, csv2):
     pass  # この行以下略
 
+
 if __name__ == '__main__':
     print(difference(csv_1, csv_2))
+    print("\n")
     print(intersect(csv_1, csv_2))
+    print("\n")
     print(union(csv_1, csv_2))
---
+++ yuki/question/question1test.py
@@ -0,0 +1,16 @@
+# 2018-2-10
+
+def main():
+    result = []
+    i = 0
+    for i in range(1,10):
+        if i % 2 == 0:
+            result.append(i)
+        i = i + 1
+    print(result)
+    return result
+
+
+main()
+
+# print(result)
