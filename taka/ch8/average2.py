61e84c68c223e0dfc0419bf51d7d8bc55c7147be
atfam <2115106u@g.hit-u.ac.jp>
2018-02-09 22:04:20 -0600

    [new]ch8

---
+++ taka/ch8/average2.py
@@ -0,0 +1,12 @@
+def main():
+    sum = 0.0
+    count =0
+    moredata = "yes"
+    while moredata[0] == "y":
+        x = eval(input("Enter a number >>"))
+        sum = sum+x
+        count = count +1
+        moredata = input("Do you want more data?(yes or no)")
+    print("\nThe number of the average is, sum/count")
+
+main()
