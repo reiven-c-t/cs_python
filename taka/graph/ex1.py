af5645aaa5e530e5c4aacb4ed8781611d698f61e
atfam <2115106u@g.hit-u.ac.jp>
2018-02-12 18:22:40 -0600

    [update] matplotlib実験

--- taka/graph/ex1.py
+++ taka/graph/ex1.py
@@ -1,9 +1,13 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-x = np.arange(0,6,0.1)
+x = np.arange(0,50,0.1)
+z = 0
+for n in range(100):
+    z = z + (np.cos(2*n-1) * x) / (2 * n -1) ** 2
 
-y = 2 * x
+
+y = 2/np.pi - 4/ np.pi * z
 
 plt.plot(x,y)
 
